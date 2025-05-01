fb990095f7fa02d2616cb307fb9fb801c86b4ad8
augustojbe <augustojbe>
2025-05-01 13:32:38 -0300

    commit classe categoria produto

--- src/main/java/com/augustojbe/lojaVirtual/model/CategoriaProduto.java
+++ src/main/java/com/augustojbe/lojaVirtual/model/CategoriaProduto.java
@@ -10,18 +10,19 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
 @Entity
-public class MarcaProduto implements Serializable {
+public class CategoriaProduto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto" )
     private Long id;
 
     @Column(name = "nome_desc", nullable = false)
     private String nomeDesc;
+
 
 }
