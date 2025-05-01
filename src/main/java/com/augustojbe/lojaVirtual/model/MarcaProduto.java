929fbb00b90f5a566479e47bccc78e8c46e1ddb5
augustojbe <augustojbe>
2025-05-01 12:19:27 -0300

    implementacao do docker-compose e model inicial

---
+++ src/main/java/com/augustojbe/lojaVirtual/model/MarcaProduto.java
@@ -0,0 +1,28 @@
+package com.augustojbe.lojaVirtual.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+@Entity
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDesc;
+
+}
